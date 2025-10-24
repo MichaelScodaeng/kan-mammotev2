057bbc3d3b5356af0c4cee014f6f4262f750e3c6
MichaelScodaeng <captainglueman@gmail.com>
2025-10-24 23:43:13 +0900

    thiti

--- sh_scripts/thin/231068_sh/later_exe/dygmamba_mercer_02.sh
+++ sh_scripts/thin/231068_sh/later_exe/dygmamba_mercer_02.sh
@@ -9,8 +9,8 @@ cd "$PBS_O_WORKDIR"
 
 source ~/.bashrc
 module purge
-module load cuda/12.8u1
-conda activate kan_mammotev2
+module load cuda/12.1
+conda activate kan_mammote
 mkdir -p sh_scripts/thin/231068_sh/sh_logs/mercer/dygmamba
 # Run your experiment - DyGMamba last dataset
 python experiment_unified.py --models "DyGMamba" --single_encoder "mercer" --datasets UNtrade \
--- sh_scripts/thin/231068_sh/later_exe/tcl_mercer_02.sh
+++ sh_scripts/thin/231068_sh/later_exe/tcl_mercer_02.sh
@@ -9,8 +9,8 @@ cd "$PBS_O_WORKDIR"
 
 source ~/.bashrc
 module purge
-module load cuda/12.8u1
-conda activate kan_mammotev2
+module load cuda/12.1
+conda activate kan_mammote
 mkdir -p sh_scripts/thin/231068_sh/sh_logs/mercer/tcl
 # Run your experiment - TCL medium datasets
 python experiment_unified.py --models "TCL" --single_encoder "mercer" --datasets UNtrade UNvote \
