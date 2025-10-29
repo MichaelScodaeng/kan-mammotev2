128fd20d9cf88991dceed42fb8ef625a4f8af40b
MichaelScodaeng <captainglueman@gmail.com>
2025-10-29 21:59:28 +0900

    kuay

--- sh_scripts/kmm_aftertune/dygmamba_kmote_01_01.sh
+++ sh_scripts/kmm_aftertune/dygmamba_kmote_01_01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -j oe
-#PBS -q GPU-1A
+#PBS -q GPU-1
 #PBS -l select=1:ngpus=1
 #PBS -M s2516027@jaist.ac.jp
 #PBS -m be
--- sh_scripts/kmm_aftertune/tgat_kmote_04.sh
+++ sh_scripts/kmm_aftertune/tgat_kmote_04.sh
@@ -3,7 +3,7 @@
 #PBS -q GPU-S
 #PBS -l select=1:ngpus=2
 #PBS -M s2516027@jaist.ac.jp
-#PBS -m bea
+#PBS -m be
 
 
 
@@ -11,7 +11,7 @@ cd "$PBS_O_WORKDIR"
 
 source ~/.bashrc
 module purge
-module load cuda/12.8u1
+module load cuda/12.1
 conda activate kan_mammote
 
 mkdir -p sh_scripts/kmm_aftertune/sh_logs/tgat
@@ -28,13 +28,13 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # UNtrade (you set 64/64/2/0.1 here)
 CUDA_VISIBLE_DEVICES=0 python experiment_unified2.py --single_encoder kan_mammote_dual_kmote --models TGAT \
   --datasets UNtrade --disable_progress_bar --num_runs 1 \
-  --expert_dim 64 --mamba_d_state 64 --mamba_expand 2 --encoder_dropout 0.1 \
+  --expert_dim 64 --mamba_d_state 64 --mamba_expand 2 --mamba_headdim 32 --encoder_dropout 0.1 \
   > sh_scripts/kmm_aftertune/sh_logs/tgat/tgat_untrade_kmote02.log 2>&1 &
 
 # Enron (you set 64/64/4/0.1 here)
 CUDA_VISIBLE_DEVICES=1 python experiment_unified2.py --single_encoder kan_mammote_dual_kmote --models TGAT \
   --datasets Enron --disable_progress_bar --num_runs 1 \
-  --expert_dim 64 --mamba_d_state 64 --mamba_expand 4 --encoder_dropout 0.1 \
+  --expert_dim 64 --mamba_d_state 64 --mamba_expand 4 --mamba_headdim 32 --encoder_dropout 0.1 \
   > sh_scripts/kmm_aftertune/sh_logs/tgat/tgat_enron_kmote02.log 2>&1 &
 
 wait
